9d28975c84f601833f192dd602d7c572a524e11a
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2019-03-03 02:00:34 +0800

    added login and graphql

--- accenture-proto/src/queries/queries.js
+++ accenture-proto/src/queries/queries.js
@@ -16,8 +16,26 @@ const getRequestsQuery = gql`
 `;
 
 const addRequestMutation = gql`
-  mutation($requester: String!, $asset: String!, $type: String!, $subject: String!, dateRequested: String!, priority: String!, status: String!, assigned: String!) {
-    addRequest(requester: $requester, asset: $asset, type: $type, subject: $subject, dateRequested: $dateRequested, priority: $priority, status: $status, assigned: $assigned) {
+  mutation(
+    $requester: String!
+    $asset: String!
+    $type: String!
+    $subject: String!
+    $dateRequested: String!
+    $priority: String!
+    $status: String!
+    $assigned: String!
+  ) {
+    addRequest(
+      requester: $requester
+      asset: $asset
+      type: $type
+      subject: $subject
+      dateRequested: $dateRequested
+      priority: $priority
+      status: $status
+      assigned: $assigned
+    ) {
       requester
       asset
       type
