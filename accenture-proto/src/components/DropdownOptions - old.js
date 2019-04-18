7b9a2f33c027a51164eb1a6ff7846a235c9a1448
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2019-04-18 17:28:25 +0800

    deleted old files

--- accenture-proto/src/components/DropdownOptions - old.js
+++
@@ -1,7 +0,0 @@
-const options1 = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" }
-];
-
-export default options1;
